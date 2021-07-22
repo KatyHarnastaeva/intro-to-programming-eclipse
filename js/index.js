a3ccf6e4d0c9e463a935aa9f7f304ff6bdd8d22f
KatyHarnastaeva <81839558+KatyHarnastaeva@users.noreply.github.com>
2021-07-21 22:27:35 -0400

    Final Project updates

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@ let today = new Date ();
 let thisYear = today.getFullYear();
 let footer = document.querySelector("footer");
 let copyright = document.createElement("P");
-copyright.innerHTML = 'Katy Harnastaeva ' + thisYear;
+copyright.innerHTML = '© Katy Harnastaeva ' + thisYear;
 footer.appendChild(copyright);
 
-let skills = [ "HTML", "JS", "Git" ,"Visual Studio", "CSS"];
+let skills = [ "HTML", "CSS", "XML", "JavaScript", "jQuery", "SQL", " MySQL"];
 let skillsSection = document.getElementById('skills');               
 let skillsList = skillsSection.getElementsByTagName("UL")[0];    
 for (let i=0; i<=skills.length-1; i++){
